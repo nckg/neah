804743ffe75617803c9afe17ccd58a46164fdb28
Nick Goris <nick@robarov.be>
2015-09-09 10:29:07 +0200

    example

---
+++ neah.js
@@ -0,0 +1,255 @@
+function Neah() {
+    this.quotes = [
+        "It's showtime!",
+        "Alright everyone, chill.",
+        "Allow me to break the ice.",
+        "I need your clothes, your boots and your motorcycle.",
+        "I did nothing. The pavement was his enemy.",
+        "Dillon! you sonofabitch.",
+        "Now listen to me very carefully.",
+        "You, are not you. You're me.",
+        "I'm going to ask you a bunch of questions, and I want to have them answered immediately.",
+        "Ha! Crom laughs at your Four Winds.",
+        "You're a funny guy, Sully, I like you. That's why I'm going to kill you last.",
+        "Bullshit.",
+        "Bullshit.",
+        "Bulllll... SHIT!!",
+        "That's bullshit.",
+        "C'mon, don't bullshit me.",
+        "You set us up! It's all bullshit! All of it!!",
+        "Of course. I'm a terminator.",
+        "Come on Bennett. Let's party.",
+        "Into the boat!",
+        "Into the tunnel!",
+        "GET TO THE CHOPPAAAAAAAA",
+        "Chill out... dickwad.",
+        "Hello, cutie pie! One of us is in deeeep trouble.",
+        "I'm pregnant.",
+        "It's not a tumor! It's not a tumor, at all.",
+        "No problemo.",
+        "Grant me revenge! And if you do not listen, then the hell with you!",
+        "The hell you will...",
+        "The hell with you.",
+        "Who the hell are you??",
+        "My name is John Kimble. And I love my car.",
+        "I am not shitting on you.",
+        "Hey, you want to be a farmer? Here's a couple of acres.",
+        "Stick around.",
+        "Knock knock.",
+        "Comó estás?",
+        "You're fired.",
+        "You're luggage.",
+        "You're a stupid.",
+        "Iced that guy... to cone a phrase.",
+        "Well that hit the spot.",
+        "To be. Or not to be... Not to be.",
+        "The ice man cometh!",
+        "Hey, lighthead! Hey, christmas tree!",
+        "Ahh, he had to split.",
+        "Doesn't anyone stay dead anymore?",
+        "Let's... kick some ice.",
+        "This is war.",
+        "To crush your enemies, see them driven before you, and hear the lamentation of the women.",
+        "If it bleeds... we can kill it.",
+        "Now this is the plan: get your ass to Mars.",
+        "What killed the dinosaurs? The ice age!",
+        "Crom!",
+        "Frosty?",
+        "Mac! MAAAAAAAC!",
+        "The riddle... of steel!",
+        "I'm not a pervert! I'm just looking for a Turbo Man doll!!",
+        "Bring the toy back, bring the toy back to the carpet, bring it back to the carpet!!",
+        "Take your toy back to the carpet. TAKE IT BACK!!",
+        "For the first time in my life, I'm... PISSED OFF!!",
+        "My name... is not Quaid!",
+        "I don't know what the problem is, but I'm sure it can be solved without resorting to violence.",
+        "But I'm ALL woman.",
+        "My nipples are very sensitive.",
+        "Feel how soft my skin is!",
+        "I do not want to touch his ass. I want to make him talk.",
+        "Ok, but make it quick. My horse is getting tired.",
+        "I need a vacation.",
+        "OH SHIT!",
+        "Shit!",
+        "Shit.",
+        "Shit!",
+        "Shit... SHIT!",
+        "SHIT!",
+        "Come on you piece of shit... fly or DIE!",
+        "We are going to play a wonderful game called who is you daddy, and what does he do?",
+        "I'm a cybernetic organism, living tissue over a metal endoskeleton.",
+        "Come with me if you want to live.",
+        "Danger is my trade.",
+        "I'm the famous comedian, Arnold Brawnschwaigger!",
+        "Now remember, I could break your neck like a chicken's.",
+        "You are mine now, you belong to me! You're not gonna have your mommies run behind you anymore and wipe your tushies. No more complaining, no more 'Mr. Kimble let me go to the bathroom', nothing! THERE IS NO BATHROOM.",
+        "Shut up.",
+        "Shut up!",
+        "SHUUUT UUUPPP!",
+        "Shut up!",
+        "SHUT AAAAAAAAAAAHHHHHPPPPPP!!!!",
+        "You should not drink and bake.",
+        "Fuck. You.",
+        "Fuck you.",
+        "Fuck you, asshole.",
+        "Fuck you.",
+        "Fuck you.",
+        "The guy's a fucking asshole.",
+        "Assholes.",
+        "Dickhead!",
+        "Scumbag.",
+        "Sluts!",
+        "Bastards!!",
+        "One. Ugly. Motherfucker.",
+        "What the HELL are you?",
+        "Who the fuck are you?",
+        "Jesus Marge! What the FUCK!!",
+        "YOU BLEW MY COVER!!",
+        "He molested, murderered, and mutilated her!",
+        "It's as satisfying to me as uh... coming is. Like having sex with a woman and coming.",
+        "Milk is for babies. When you grow up you have to drink beer!",
+        "FREEZE IN HELL BATMAAAAAN!",
+        "You killed my father. Big mistake.",
+        "You've just been erased.",
+        "You are TERMINATED!",
+        "See you at the party, Richter!",
+        "Freeze well!",
+        "Excuse me- don't disturb my friend. He's dead tired.",
+        "Hey, you can't do that!",
+        "Wrong.",
+        "When the governor gets here, call me.",
+        "I eat green berets for breakfast. And right now... I'm VERY HUNGRY.",
+        "You've seen those movies where they say 'make my day' or 'I'm your worst nightmare'? Well listen to this one... rubber baby buggy bumpers.",
+        "Ha hahaha ha ha! You think this is the real Quaid? It is.",
+        "You're not sending ME to the cooler...",
+        "I have my orders.",
+        "Hey, I'm a police officer. This is an arrest.",
+        "I'm a cop you eediot! I'm Detective John Kimble, this man is under arrest.",
+        "I'm the party pooper.",
+        "Remember Sully when I promised to kill you last? I lied.",
+        "No sequel for you.",
+        "I'll be back.",
+        "I'll... be back.",
+        "I'll be back.",
+        "I'll be back.",
+        "I'll be BACK.",
+        "I'll be back.",
+        "I'm back.",
+        "BWA HAHAHAHAHA!",
+        "Ahahahaha!",
+        "Get down.",
+        "Get down!",
+        "Get down!",
+        "Get down!",
+        "Get DOWN!!!",
+        "Get down!!",
+        "Get down, or I'll put you down.",
+        "Get out.",
+        "Get out.",
+        "Get out.",
+        "Get down.",
+        "Hasta la vista. Baby.",
+        "Let off some steam, Bennett.",
+        "Here is Subzero. Now... Plain Zero.",
+        "Just bodies.",
+        "Come on, Cohaagen! You got what you want. Give those people air!!",
+        "What about the guy you lobotomized? Did he get a refund?",
+        "Relax. You'll live longer.",
+        "Do it. DO IT! Come on, kill me, I'm here! COME ON, DO IT NOW, KILL MEEEE!!",
+        "WOOOAAAHHHHHHHHHHHH",
+        "ARRRRRRRGGGGGGGH",
+        "GAAAAAAGGHHHHHH!",
+        "WWWAAAAAGGHHHHH",
+        "AAAAARRRRRGGGHHHHHHHHH",
+        "AAAAAAAAHHHHHHHHHH!!!!",
+        "GAAAAAAAHHHHHH",
+        "AAAAAAAAHHHHHHHHH",
+        "GWWWOOOHHHHHH",
+        "YYYAAAAAAAAAAAAAAAAAAAAAAAAAAAGGGHHHHHHHHH",
+        "Put the cookie down. NOW!!",
+        "Cookies? Who told you you could eat my cookies?",
+        "You want to fuck with me? You think you know bad, huh? You're a fucking CHOIR BOY to me. A CHOIR BOY.",
+        "SCREW YOOOOUUU",
+        "You cold blooded bastard... I'll tell you what I think of it. I live to see you eat that contract. But I hope you leave enough room for my fist, because I'm going to ram it into your stomach, and break your god damn spine!!!",
+        "Could I speak to the drug dealer of the house please? It's a beautiful day and we're out killing drug dealers.",
+        "This is all a lie. I was framed. I am completely innocent!",
+        "Hercules doesn't need any money.",
+        "Hercules goes where he wishes.",
+        "I'm afraid that my position has left me... cold to your pleas of mercy.",
+        "If I'm not me, who the hell am I?",
+        "You have no respect for logic. And I have no respect for those with no respect to logic.",
+        "Adam... and Evil.",
+        "Your confusion is not rational. She is a healthy female of breeding age.",
+        "What the hell is going on here? There's someone in my house, eating my birthday cake, and it's not me.",
+        "I saved a life today, what the hell did you do?",
+        "You took my father's sword!",
+        "Knives. And stabbing weapons.",
+        "What's the matter? The CIA got you pushing too many pencils?",
+        "I remember days like this, when my father took me to the forest and we ate wild blueberries.",
+        "If revenge is a dish best served cold... then put on your Sunday's finest, it's time to feast!",
+        "Sue me, dickhead.",
+        "When I said you should screw yourself... I didn't mean for you to take it literally.",
+        "Try to stay dead this time.",
+        "You have striked Hercules.",
+        "It's just a doll... it's just a stupid little plastic doll.",
+        "Kill the heroes! Yes, kill them, kill them, yes!",
+        "You picked the wrong day.",
+        "Fine chariot, but where are ze horses?",
+        "It's turbo time.",
+        "NOOOO! Please, anything but my statue of me!",
+        "Gee, did you make a movie mistake? You forgot to reload the damn gun!",
+        "Ok, enough philosophy. I've got to get going."
+    ];
+    this.quoteInParagraph = 10;
+}
+
+Neah.prototype.getRandom = function() {
+    return this.quotes[Math.floor(Math.random() * this.quotes.length)];
+};
+
+Neah.prototype.paragraph = function() {
+    var paragraph = [];
+
+    for (var i = 0; i < this.quoteInParagraph; i++) {
+        paragraph.push(this.getRandom());
+    };
+
+    return paragraph.join(' ');
+};
+
+Neah.prototype.paragraphs = function(count) {
+    var paragraphs = [];
+
+    for (var i = 0; i < count; i++) {
+        paragraphs.push(this.paragraph());
+    };
+
+    return paragraphs.join( "\n\n" );
+};
+
+(function(window){
+
+
+if ( typeof module === "object" && module && typeof module.exports === "object" ) {
+    // Expose Neah as module.exports in loaders that implement the Node
+    // module pattern (including browserify). Do not create the global, since
+    // the user will be storing it themselves locally, and globals are frowned
+    // upon in the Node module world.
+    module.exports = Neah;
+} else {
+    // Otherwise expose Neah to the global object as usual
+    window.Neah = Neah;
+
+    // Register as a named AMD module, since Neah can be concatenated with other
+    // files that may use define, but not via a proper concatenation script that
+    // understands anonymous AMD modules. A named AMD is safest and most robust
+    // way to register. Lowercase Neah is used because AMD module names are
+    // derived from file names, and Neah is normally delivered in a lowercase
+    // file name. Do this after creating the global so that if an AMD module wants
+    // to call noConflict to hide this version of Neah, it will work.
+    if ( typeof define === "function" && define.amd ) {
+        define( "neah", [], function () { return Neah; } );
+    }
+}
+
+})(this);
